b11a3e34823b4913a77688da23a43da6e3847556
Sidharth Soman <sidharthsoman065@gmail.com>
2025-06-03 18:25:26 +0530

    change in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import { BrowserRouter as Router,Routes,Route,Link } from 'react-router-dom';
+import { BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import './App.css'
 import Header from './components/header-component/Header';
 import Home from './components/home-component/Home';
