efd139b2f20039b6fcb48e9a5696950a08e0ef87
Sidharth Soman <sidharthsoman065@gmail.com>
2025-06-09 17:00:57 +0530

    styled the home component

--- src/components/home-component/Home.jsx
+++ src/components/home-component/Home.jsx
@@ -6,29 +6,40 @@ import Header from "../header-component/Header";
 
 const Home = () => {
   const [gameComponent, setGameComponent] = useState(false);
-  
+
   const showGameComponent = () => {
     setGameComponent(true);
   };
 
   return (
     <>
-      <div>
-        <Header />
-        {!gameComponent && (
-          <div className="flex flex-col items-center justify-center h-screen">
-            <div className="border border-indigo-600 w-1/2 h-1/2">
-              <div className="p-5 ">
-                <h1 className="text-2xl">Hello this could be the heading</h1>
-                <p className="mt-10">Hai this is the paragraph which is the thing</p>
-              </div>
-              <div className="flex items-center justify-center h-full">
-                <button type="button" onClick={showGameComponent}>Click Here</button>
-              </div>
+      <Header />
+      <div className="h-screen w-full flex flex-col items-center justify-center">
+        {!gameComponent ? (
+          // <div className="bg-white max-w-md mx-auto rounded-3xl shadow-2xl text-center flex flex-col items-center justify-center p-10 md:max-w-2xl sm:h-full">
+          <div className="bg-white max-w-md md:max-w-2xl mx-auto rounded-3xl shadow-2xl text-center flex flex-col items-center justify-center p-10 h-full sm:h-auto">
+          <div className="p-15">
+              <h1 className="text-4xl font-extrabold text-gray-800 pb-5 drop-shadow-sm">
+                Welcome to the Game
+              </h1>
+              <p>
+                Unscramble the letters to test your brainpower. Are you ready to
+                play?
+              </p>
+            </div>
+            <div>
+              <button
+                type="button"
+                onClick={showGameComponent}
+                className="px-8 py-3 bg-indigo-600 text-white text-xl font-medium rounded-full hover:bg-indigo-700 transition duration-300 shadow-lg"
+              >
+              🚀 Start Game 🚀 
+              </button>
             </div>
           </div>
+        ) : (
+          <Game isStarted={gameComponent} />
         )}
-        {gameComponent && <Game isStarted={gameComponent} />}
       </div>
     </>
   );
