033d31782eacfe4544150429688f5b7cdf6b5f75
Sidharth Soman <sidharthsoman065@gmail.com>
2025-06-09 18:12:30 +0530

    added failure animation

--- src/components/result-component/Result.jsx
+++ src/components/result-component/Result.jsx
@@ -1,14 +1,16 @@
 import Lottie from 'lottie-react'
 import React from 'react'
 import successJson from '../../assets/animations/success.json'
-
+import failJson from '../../assets/animations/faliure.json'
 
 const Result = ({isTrue}) => {
   return (
     <>
     {isTrue ? (<div className='w-64 h-64'>
     <Lottie animationData={successJson}/>
-  </div>) : <></>}
+  </div>) : (<div className='w-64 h-64'>
+    <Lottie animationData={failJson}/>
+  </div>)}
     </>
   )
 }
